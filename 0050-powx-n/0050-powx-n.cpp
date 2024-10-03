ce8f3cf7e9027ab359b5f664f0bd042e9aee31ba
Ankit Sharma <113293888+ankitsharma495@users.noreply.github.com>
2024-10-03 10:37:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (42.67%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  double myPow(double x, int n) {
+    
+        if(n==0){
+            return 1;
+        }
+       
+        double temp = myPow(x,n/2);
+        temp = temp*temp;
+       if(n%2==0){
+        return temp;
+       
+       } 
+       else{
+        if(n>0){
+            return x*temp;
+
+        } 
+        if(n<0){ 
+            return (1 / x) * temp;
+            }
+       }
+       return temp;
+    }
+};
