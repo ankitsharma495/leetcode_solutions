8160769e3ff12b33a8c27b52eaf925473eb1ac4e
Ankit Sharma <113293888+ankitsharma495@users.noreply.github.com>
2026-07-19 22:17:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.99%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int last[26];
+        bool seen[26] = {};
+
+        for (int i = 0; i < s.size(); i++)
+            last[s[i] - 'a'] = i;
+
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+
+            if (seen[c - 'a'])
+                continue;
+
+            while (!ans.empty() &&
+                   ans.back() > c &&
+                   last[ans.back() - 'a'] > i) {
+
+                seen[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(c);
+            seen[c - 'a'] = true;
+        }
+
+        return ans;
+        
+    }
+};
